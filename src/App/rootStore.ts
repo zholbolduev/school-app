7a4671d6c6a1d5cd1eb9c7dba62e6f742669bea8
edilk <ed2803k@gmail.com>
2023-07-12 21:55:44 +0600

    Updated

---
+++ src/App/rootStore.ts
@@ -0,0 +1,16 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import newCourseReducer from "../Features/AddCourse/NewCourseSlice";
+
+const rootReducers = combineReducers({
+  newCourseReducer,
+});
+
+export const setupStore = () => {
+  return configureStore({
+    reducer: rootReducers,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducers>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
