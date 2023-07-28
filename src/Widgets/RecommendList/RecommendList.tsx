21a2208639247d02f2f40ad8a3a8f47d82499c89
Alibek <musicchannel0p@gmail.com>
2023-07-28 18:13:13 +0600

    made free course list and main pahe

---
+++ src/Widgets/RecommendList/RecommendList.tsx
@@ -0,0 +1,42 @@
+import CourseCard from "../../Entities/Cards/CourseCard";
+import "./RecommendList.scss";
+
+const RecommendList = () => {
+  const cards = [
+    {
+      id: 1,
+      title: "JS",
+      description: "Interpersonal skills - work better with others!",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtME2Ho74uhChIuase5oqeJujVV-wmBEAWAg&usqp=CAU",
+      price: "300",
+      type: "paid",
+    },
+    {
+      id: 2,
+      title: "JS",
+      description: "Interpersonal skills - work better with others!",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtME2Ho74uhChIuase5oqeJujVV-wmBEAWAg&usqp=CAU",
+      price: "300",
+      type: "paid",
+    },
+    {
+      id: 3,
+      title: "JS",
+      description: "Interpersonal skills - work better with others!",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtME2Ho74uhChIuase5oqeJujVV-wmBEAWAg&usqp=CAU",
+      price: "300",
+      type: "paid",
+    },
+  ];
+  return (
+    <div className="recommendList">
+      <div className="recommendList__wrapper">
+        {cards.map((card) => (
+          <CourseCard key={card.id} card={card} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecommendList;
